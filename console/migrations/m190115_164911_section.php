4910d1bf27564881eefd993defde9184c0ac6a29
zapata666 <cartel_1697@hotmail.com>
2019-01-15 11:51:29 -0500

    Cambios en tabla de lecciones

---
+++ console/migrations/m190115_164911_section.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190115_164911_section
+ */
+class m190115_164911_section extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+            return $this->execute(file_get_contents(Yii::getAlias('console/migrations/sql/m190115_164911.sql')));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190115_164911_section cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190115_164911_section cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ console/migrations/sql/m190115_164911.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `lesson` 
+CHANGE COLUMN `sections_id` `section_id` INT(11) NOT NULL ;
