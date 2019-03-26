8dc270827877cf6a9685a242d3a9dd84b3af0251
AndreyPootMay <spocker312@gmail.com>
2019-03-26 13:13:19 -0500

    Traducciones

--- frontend/views/my-courses/update.php
+++ frontend/views/my-courses/update.php
@@ -5,17 +5,16 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\MyCourses */
 
-$this->title = Yii::t('app', 'Update My Courses: {name}', [
+$this->title = Yii::t('app', 'Editar el curso: {name}', [
     'name' => $model->id,
 ]);
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'My Courses'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Mis cursos'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+$this->params['breadcrumbs'][] = Yii::t('app', 'Editar');
 ?>
 <div class="my-courses-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
